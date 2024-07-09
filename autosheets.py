6dcad983c72945b764480f34488b46050c8c9215
drestanto <dyas@live.com>
2024-07-09 09:18:03 +0700

    env example

--- autosheets.py
+++ autosheets.py
@@ -26,17 +26,8 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    brooklyn_99_quotes = [
-        'I\'m the human form of the 💯 emoji.',
-        'Bingpot!',
-        (
-            'Cool. Cool cool cool cool cool cool cool, '
-            'no doubt no doubt no doubt no doubt.'
-        ),
-    ]
-
-    if message.content == '99!':
-        response = random.choice(brooklyn_99_quotes)
-        await message.channel.send(response)
+    print(message.content)
+    if (len(message.attachments)!=0):
+        print(message.attachments[0].url)
 
 client.run(TOKEN)
